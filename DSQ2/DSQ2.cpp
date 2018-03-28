8f6f1cc55913f9e456e061e3ccbc6e9451a5d733
wanghaorangithub <wanghaorangithub@users.noreply.github.com>
2018-03-28 18:31:49 +0800

    完成了mystack.h

--- DSQ2/DSQ2.cpp
+++ DSQ2/DSQ2.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
-
-
+#include"mystack.h"
+#include <string> 
+using namespace std;
 int main()
 {
+	MyStack <int> mystack;
+	mystack.push(1);
+	cout << mystack.getTop() << endl;
+	cout << mystack.pop() << endl;
+	cout << mystack.isEmpty();
     return 0;
 }
 
